76102f8bc9ea626d50efa508cd360c75a3205ed7
Trevor Sundberg <Trevor.Sundberg@gmail.com>
2020-10-18 14:36:07 -0700

    Tests now running wasm2brs

--- test/index.ts
+++ test/index.ts
@@ -3,17 +3,58 @@ import path from "path";
 import execa from "execa";
 import mkdirp from "mkdirp";
 
+interface WastModule {
+  type: "module";
+  filename: string;
+}
+
+interface WastAssertReturn {
+  type: "assert_return";
+  filename: string;
+  action: {
+    type: "invoke";
+    field: string;
+    args: {
+      type: "i32" | "i64";
+      value: string;
+    }[]
+  };
+}
+
+interface WastJson {
+  commands: (WastModule | WastAssertReturn)[];
+}
+
 (async () => {
   const root = path.join(__dirname, "../..");
   const out = path.join(root, "test/out");
+  const fromRootOptions: execa.Options = {cwd: root, stdio: "inherit"};
   await mkdirp(out);
+
+  const outJson = path.join(out, "address.json");
   await execa("third_party/wabt/bin/wast2json",
     [
       "third_party/wabt/third_party/testsuite/address.wast",
-      "-o", path.join(out, "best.json")
+      "-o", outJson
     ],
-    {cwd: root});
+    fromRootOptions);
 
-  const tests = JSON.parse(fs.readFileSync("/home/trevor/wabt/address.json", "utf8"));
-  console.log(tests.commands);
+  const wastJson = JSON.parse(fs.readFileSync(outJson, "utf8")) as WastJson;
+
+  for (const command of wastJson.commands) {
+    switch (command.type) {
+      case "module":
+        console.log("Module", command.filename);
+        await execa("build/wasm2brs",
+          [
+            "-o", "project/source/wasm.brs",
+            path.join(out, command.filename)
+          ],
+          fromRootOptions);
+
+        break;
+      case "assert_return":
+        break;
+    }
+  }
 })();
