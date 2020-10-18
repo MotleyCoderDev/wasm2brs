f82a5d9a89bd0859efcf1f353052b34a1ed19eae
Trevor Sundberg <Trevor.Sundberg@gmail.com>
2020-10-18 13:21:20 -0700

    Basic tests outputting

--- test/index.ts
+++ test/index.ts
@@ -1,10 +1,18 @@
 import fs from "fs";
 import path from "path";
 import execa from "execa";
+import mkdirp from "mkdirp";
 
 (async () => {
-  const root = path.join(__filename, "../..");
-  await execa("third_party/wabt/bin/wast2json", ["third_party/wabt/third_party/testsuite/address.wast"], {cwd: root});
+  const root = path.join(__dirname, "../..");
+  const out = path.join(root, "test/out");
+  await mkdirp(out);
+  await execa("third_party/wabt/bin/wast2json",
+    [
+      "third_party/wabt/third_party/testsuite/address.wast",
+      "-o", path.join(out, "best.json")
+    ],
+    {cwd: root});
 
   const tests = JSON.parse(fs.readFileSync("/home/trevor/wabt/address.json", "utf8"));
   console.log(tests.commands);
