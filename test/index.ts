f17e34d83dcfce93398c06d9b5e7cc66dd594040
Trevor Sundberg <Trevor.Sundberg@gmail.com>
2020-11-22 15:29:13 -0800

    Emit other requirements for the manifest file

--- test/index.ts
+++ test/index.ts
@@ -268,7 +268,16 @@ const outputWastTests = async (wastFile: string, guid: string): Promise<boolean 
     fs.writeFileSync(testWasmBrs, testWasmFile);
   }
 
-  fs.writeFileSync(path.join(project, "manifest"), `title=${guid}`);
+  fs.writeFileSync(path.join(project, "manifest"),
+    `title=${guid}\n` +
+    "major_version=1\n" +
+    "minor_version=0\n" +
+    "build_version=0\n" +
+    "mm_icon_focus_hd=pkg:/images/mm_icon_focus_hd.png\n" +
+    "mm_icon_focus_sd=pkg:/images/mm_icon_focus_sd.png\n" +
+    "splash_screen_hd=pkg:/images/splash_screen_hd.png\n" +
+    "splash_screen_sd=pkg:/images/splash_screen_sd.png\n"
+  );
   return true;
 };
 
