5098a975570e5c98c6087defd5381c2fad8380db
Trevor Sundberg <Trevor.Sundberg@gmail.com>
2020-11-21 21:46:11 -0800

    Update JavaScript printing to match browser

--- samples/javascript/main.cc
+++ samples/javascript/main.cc
@@ -12,6 +12,28 @@ const char* intro =
     "  ({test: 10*10})\n"
     "  (function(){var a=3;return a*2})()\n";
 
+void print_top_and_pop(duk_context* ctx) {
+    duk_idx_t top = duk_get_top(ctx);
+    
+    // Try first to turn it into JSON if it's an Object, and if that fails then use String(...)
+    duk_dup_top(ctx);
+    const char* result = nullptr;
+    if (duk_get_type(ctx, -1) == DUK_TYPE_OBJECT) {
+        result = duk_json_encode(ctx, -1);
+        if (result == nullptr) {
+            duk_pop(ctx);
+        }
+    }
+    if (result == nullptr) {
+        duk_get_global_string(ctx, "String");
+        duk_swap_top(ctx, -2);
+        duk_call(ctx, 1);
+        result = duk_to_string(ctx, -1);
+    }
+    std::cout << "Result: " << (result ? result : "undefined") << std::endl;
+    duk_pop_n(ctx, duk_get_top(ctx) - top + 1);
+}
+
 int main(void) {
     duk_context* ctx = duk_create_heap_default();
     std::cout << intro;
@@ -19,9 +41,7 @@ int main(void) {
         std::string input;
         std::getline(std::cin, input);
         duk_eval_string(ctx, input.c_str());
-        const char* json = duk_json_encode(ctx, -1);
-        std::cout << "Result: " << (json ? json : "undefined") << std::endl;
-        duk_pop(ctx);
+        print_top_and_pop(ctx);
     }
     return 0;
 }
