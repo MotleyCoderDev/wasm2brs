cde0e3834ad13e36be8fc1652214c7051e9bd202
Trevor Sundberg <Trevor.Sundberg@gmail.com>
2020-11-24 20:44:13 -0800

    Add seeking file sample

--- samples/files/files.cc
+++ samples/files/files.cc
@@ -4,6 +4,14 @@
 int main(void) {
     FILE* fp = fopen("pkg:/manifest", "r");
     printf("fp: %p\n", fp);
+    fseek(fp, 0, SEEK_END);
+    long size = ftell(fp);
+    fseek(fp, 0, SEEK_SET);
+    printf("file size: %d\n", (int)size);
+
+    printf("skipping first character\n");
+    fseek(fp, 1, SEEK_CUR);
+
     char buffer[4096] = {0};
     fread(buffer, 1, sizeof(buffer) - 1, fp);
     printf("%s\n", buffer);
