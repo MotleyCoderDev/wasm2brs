b21831b83d6ef34b8bc8b3d8d2216236b8801512
Trevor Sundberg <Trevor.Sundberg@gmail.com>
2020-12-06 16:17:20 -0800

    Support colors in output

--- make.sh
+++ make.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
 docker build -q -t wasm2brs ./docker
-docker run -v `pwd`:`pwd` -w `pwd` -u `id -u`:`id -g` --network host --rm wasm2brs make "$@"
+docker run -t -v `pwd`:`pwd` -w `pwd` -u `id -u`:`id -g` --network host --rm wasm2brs make "$@"
