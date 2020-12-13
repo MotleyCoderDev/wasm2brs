2417c9ff331810df2b25d5c0115f22ac9ad9aff3
Trevor Sundberg <Trevor.Sundberg@gmail.com>
2020-12-13 13:23:54 -0800

    Updated roku.h documentation

--- include/roku.h
+++ include/roku.h
@@ -5,12 +5,30 @@
 #ifndef ROKU_H
 #define ROKU_H
 
+/*
+    Create a singleton global surface.
+    If bitsPerPixel = 8, then you must call roku_set_surface_colors before roku_draw_surface.
+*/
 void roku_create_surface(int bitsPerPixel, int width, int height);
 
-void roku_set_surface_colors(void* colorTableOffset);
+/*
+    Set the color palette for a created surface. Only valid when bitsPerPixel = 8.
+*/
+void roku_set_surface_colors(void* colorTable);
 
-void roku_draw_surface(void* pixelDataOffset);
+/*
+    Render the surface to the screen with the given pixel data.
+    The pixelData must be of size width * height * (bitsPerPixel / 8).
+*/
+void roku_draw_surface(void* pixelData);
 
+/*
+    Poll to see if any buttons are pressed, released, or held.
+    When pressed, the value will match roku_button (e.g. 6 for ROKU_OK).
+    When released, the value will be roku_button + 100 (e.g. 106 for ROKU_OK).
+    When held / repeated, the value will be roku_button + 1000 (e.g. 1006 for ROKU_OK).
+    Returns ROKU_INVALID if there are no button events.
+*/
 unsigned int roku_poll_button(void);
 
 enum roku_button {
