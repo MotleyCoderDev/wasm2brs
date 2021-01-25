7efe434915361073a9afe946a1fe8db713459fe3
Trevor Sundberg <Trevor.Sundberg@gmail.com>
2021-01-24 17:30:40 -0800

    Fix build under root user / sudo

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 set -e
 cd "$(dirname "$0")"
 docker build -t wasm2brs ./docker
-docker run -t -v `pwd`:`pwd` -w `pwd` -u `id -u`:`id -g` --network host --rm wasm2brs "$@"
+docker run -t -v `pwd`:`pwd` -w `pwd` -u 1000:1000 --network host --rm wasm2brs "$@"
